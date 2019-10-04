897ac0bb675f592677f50cccdfcfb38745a04ad0
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-10-03 20:11:20 -0600

    Got the interaction between board and tile working

--- board.rb
+++ board.rb
@@ -1,6 +1,15 @@
-require 'tile'
+require_relative 'tile'
 
 #Start out with a 9x9 grid
 class Board
+
+    attr_reader :grid
+
+    def initialize(size=9)
+        @size = size
+        @grid = Array.new(size) { Array.new(size) {Tile.new(self)} }
+    end
+
+    #13 bombs spread out over 81 squares randomly
 
 end
--- tile.rb
+++ tile.rb
@@ -1,6 +1,26 @@
 #Things to track: bombed? flagged? revealed?
 class Tile
-    def initialize(board_object)
+
+    def initialize(board_object, bombs=13)
+        @board = board_object
+        @bombs = bombs
+        @bombed = false
+        @flagged = false
+        @revealed = false
+    end
+
+    def randomize
+        rows = @board.grid.length
+        cols = @board.grid[0].length
+
+        while i < 13
+            new_row = rand(9)
+            new_col = rand(9)
+
+            if @board.grid[new_row][new_col] != "B"
+
+            end
+        end
 
     end
 
