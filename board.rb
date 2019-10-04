03b21af0aa8fd572ef5af139f208d16f1d55b5d7
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-10-03 20:43:39 -0600

    Got bombs logic working

--- board.rb
+++ board.rb
@@ -3,7 +3,7 @@ require_relative 'tile'
 #Start out with a 9x9 grid
 class Board
 
-    attr_reader :grid
+    attr_reader :grid, :size
 
     def initialize(size=9)
         @size = size
@@ -11,5 +11,44 @@ class Board
     end
 
     #13 bombs spread out over 81 squares randomly
+    def place_bombs
+        number_bombs = ((@size**2)/(100/16)).floor
+
+        until number_bombs == 0
+            new_row = rand(8)
+            new_col = rand(8)
+
+            if self.[](new_row, new_col).bomb_bool == false
+                self.[](new_row, new_col).bomb_bool = true
+                number_bombs -= 1
+            end
+        end
+    end
+
+    def render
+        system("clear")
+        puts "  #{(0...@grid.length).to_a.join('  ')}"
+        @grid.each_with_index do |row, i|
+            puts "#{i} #{row}"
+        end
+    end
+
+    #important method that will allow you to take 2 ints and access the grid
+    #using nested array syntax
+    def [](pos1, pos2)
+        if valid_pos?(pos1, pos2)
+            @grid[pos1][pos2]
+        else
+            puts "Invalid position"
+        end
+    end
+
+    def valid_pos?(pos1, pos2)
+        if pos1 < @size && pos2 < @size
+            return true
+        else
+            return false
+        end
+    end
 
 end
--- tile.rb
+++ tile.rb
@@ -1,39 +1,32 @@
 #Things to track: bombed? flagged? revealed?
 class Tile
 
+    attr_accessor :bomb_bool, :flag_bool, :reveal_bool
+
     def initialize(board_object, bombs=13)
         @board = board_object
-        @bombs = bombs
-        @bombed = false
-        @flagged = false
-        @revealed = false
+        @bomb_bool = false
+        @flag_bool = false
+        @reveal_bool = false
     end
 
-    def randomize
-        rows = @board.grid.length
-        cols = @board.grid[0].length
-
-        while i < 13
-            new_row = rand(9)
-            new_col = rand(9)
-
-            if @board.grid[new_row][new_col] != "B"
-
-            end
+    def inspect
+        if @bomb_bool
+            return "B"
+        elsif @flag_bool
+            return "F"
+        else
+            return "*"
         end
-
     end
-
 
     def reveal
 
     end
 
-
     def neighbors
 
     end
-
 
     def neighbor_bomb_count
 
